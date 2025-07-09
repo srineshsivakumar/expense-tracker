4aa9959d0882caa650c319dd8fbad6cf6c143c53
naveen-166 <naveenramce.22aid@kongu.edu>
2025-07-09 09:50:33 +0530

    coumt3

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -1,14 +1,14 @@
-const { Router } = require('express')
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
+const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
 
-const router=require('express').Router()
-
-const {addIncome, getIncomes, deleteIncome} = require('../controllers/income')
-
+const router = require('express').Router();
 
 
+router.post('/add-income', addIncome)
+    .get('/get-incomes', getIncomes)
+    .delete('/delete-income/:id', deleteIncome)
+    .post('/add-expense', addExpense)
+    .get('/get-expenses', getExpense)
+    .delete('/delete-expense/:id', deleteExpense)
 
-router.post('/add-income',addIncome)
-    .get('/get-income',getIncomes)
-    .delete('/delete-income/:id',deleteIncome)
-
-module.exports=router
+module.exports = router
