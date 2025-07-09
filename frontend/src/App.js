b8a53a2bf2d12122545da026ae78cfdacf344b00
naveen-166 <naveenramce.22aid@kongu.edu>
2025-07-09 16:02:51 +0530

    count3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import bg from './img/bg.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyled bg={bg} className="App">
+
+    </AppStyled>
   );
 }
+
+  const AppStyled = styled.div`
+    height: 100vh;
+    background-image: url(${props=>props.bg})
+  `;
 
 export default App;
